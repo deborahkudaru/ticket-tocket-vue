e91de5ee942e119709efa6168ce6a5441758cda0
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-10-25 18:02:20 +0100

    create auth for login and signup session

---
+++ src/context/AuthProvider.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { provide } from 'vue'
+import { useAuthProvider } from '../composables/useAuth.js'
+
+const auth = useAuthProvider()
+provide('auth', auth)
+</script>
+
+<template>
+  <slot />
+</template>
